fd6b4152706aaa171b1491b24b66f15ac12b5677
Diego Tobar <dtobar@veritran.com>
2023-11-02 18:25:41 -0500

    Iteraciones

---
+++ Swift1.playground/Pages/Iteraciones.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+for i in 0...10 {
+    print("Iteracion \(i)")
+}
+
+
+for i in stride(from: 0, to: 10, by: 2) {
+    print("Iteracion \(i)")
+}
+
+
+for i in stride(from: 0, through: 10, by: 2) {
+    print("Iteracion \(i)")
+}
+
+
+let x = stride(from: 0, to: 10, by: 2)
